15c3c88cb4afcbdb57fcfc19005eb3a4df147ded
Kiprotich Kipchumba <96067114+DeenRamah@users.noreply.github.com>
2024-07-17 10:35:11 +0300

    Create setIntervalsyntax.js

---
+++ setIntervalsyntax.js
@@ -0,0 +1,4 @@
+//The setInterval javascript method is used to call a function repeatedly at a specified interval of time
+let intervalID = setInterval(func, delay)
+//funct: the first parameter is a function to be executted
+//delay: indicates time in milliseconds i.e the interval at which the function is to be called
