dbb34380c47f7eb5210791c24c238d1e345df3f1
Kiprotich Kipchumba <96067114+DeenRamah@users.noreply.github.com>
2024-07-17 09:44:49 +0300

    Create setTimeoutsyntax.js

---
+++ setTimeoutsyntax.js
@@ -0,0 +1,5 @@
+//syntax
+//these method executes a function,after waiting a specified number of milliseconds
+let timeID = setTimeout(func, delay)
+//func:the first parameter is a function to be executed
+//delay:indicates the time in millisecond after which the above function is to be called
