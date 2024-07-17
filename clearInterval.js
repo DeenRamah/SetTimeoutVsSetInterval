7ee4e0b7cc3fadeff54c72cc1ff0068aeea3ad9a
Kiprotich Kipchumba <96067114+DeenRamah@users.noreply.github.com>
2024-07-17 10:37:50 +0300

    Create clearInterval.js

---
+++ clearInterval.js
@@ -0,0 +1,3 @@
+// since setInterval() method executes the function infinetely hence there is a method called as clearInterval() to stop the execution of the setInterval()
+let intervalID = setnterval(...);
+clearInterval(inervalID);
