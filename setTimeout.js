4dc256f2b0224b175fa3eef3441234ec7cbd7763
Kiprotich Kipchumba <96067114+DeenRamah@users.noreply.github.com>
2024-07-17 09:54:48 +0300

    Create setTimeout.js

---
+++ setTimeout.js
@@ -0,0 +1,9 @@
+<p>Press the stop button before the alert is shown</p>
+<button onclick="val = setTimeout(printMyName, 2000)">
+  print User Name
+  </button>
+<button onclick="clearTimeout(val);">Stop Execution</button>
+
+<script>
+  function printMyname()
+  </script>
